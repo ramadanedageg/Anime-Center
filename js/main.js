c20f130307dc8057a7f83d18166c452e45112d0a
ramadanedageg <75795809+ramadanedageg@users.noreply.github.com>
2021-01-02 20:43:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* First Slider */
+$('.slider-one')
+.not(".slick-intialized")
+.slick({
+    autoplay : true,
+    autoplayspeed : 3000,
+    dots : true,
+    prevArrow : ".site-slider .slider-btn .prev",
+  nextArrow : ".site-slider .slider-btn .next",
+});
+
+/* Second Slider */
+$('.slider-two')
+.not(".slick-intialized")
+.slick({
+    prevArrow : ".site-slider-two .prev",
+    nextArrow : ".site-slider-two .next",
+    slidesToShow : 5,
+    slidesToScroll : 1,
+    autoplayspeed : 3000,
+    autoplay : true,
+});
